2888606ba5e142b5fe55ee0dd2d533111ac6f83a
glauco.todesco <glauco.todesco@facens.br>
2021-02-09 18:21:03 -0300

    Create Project, Header and Home Component

---
+++ src/core/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import './styles.scss';
+
+const Header = () => (
+  <div className="headear-container">
+    <div className="header-title">
+        Bootcamp DevSuperior
+     </div>
+  </div>
+);
+
+export default Header;
+
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,20 @@
+import './styles.scss';
+
+const Home = () => (
+  <div className="home-container" >
+    <div className="home-card">
+      <div className="home-title">
+        Desafio do Capítulo 3, <br />
+        Bootcamp DevSuperior
+      </div>
+      <div className="home-text">
+        <p>Bem-vindos ao desafio do capítulo 3 do Bootcamp DevSuperior.</p>
+        <p>Favor observar as instruções passadas no capítulo sobre a elaboração deste projeto.</p>
+        <p>Este design foi adaptado a partir de Ant Design System for Figma, de Mateusz Wierzbicki: antforfigma@gmail.com</p>
+      </div>
+    </div>
+  </div>
+);
+
+export default Home;
+
